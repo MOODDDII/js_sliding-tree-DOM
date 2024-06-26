d118a543b968b6df461692a756287255ad78bd91
Marian <getmoodyup@gmail.com>
2024-06-26 10:37:47 +0300

    make build

---
+++ index.4ff8ad35.js
@@ -0,0 +1,2 @@
+document.querySelector(".tree").addEventListener("click",function(e){var l,t=e.target.closest("li");t&&(l=t.querySelector("ul"))&&(l.style.display="none"===l.style.display||""===l.style.display?"block":"none")});
+//# sourceMappingURL=index.4ff8ad35.js.map
